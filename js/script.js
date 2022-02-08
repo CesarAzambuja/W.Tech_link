5a1fd22ca8606acb99d8c20d1ae7ad3109c73cc3
César Oliveira <cesar.oliveira@takelog.com.br>
2022-02-08 09:54:16 -0300

    Inicial

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function alert(){
+    swal("Informativo COVID", "Devido a pandemia do Covid-19, os atendimentos presenciais estão suspensos. Assim que possível, informarei sobre o retorno através das minhas redes sociais.");
+}
